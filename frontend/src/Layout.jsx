13ede0ed3ed4b7c4c3f968b175b94f6007a2bab5
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-13 15:59:50 -0300

    frontend: implemented react-router-dom and initial layout design

---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+const Layout = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-r from-[#e75858] to-[#d86023]">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,77 +1,72 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="bg-white/95 border-b border-orange-100 sticky top-0 z-50">
-      <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
-          <a href="/">
-            <div className="w-14 h-14 bg-gradient-to-r from-orange-500 to-red-500 rounded-full flex items-center justify-center">
-              <span className="text-white font-bold text-sm">MIAM</span>
-            </div>
-          </a>
-
-          <div className="hidden md:flex items-center space-x-8">
-            <a
-              href="#"
-              className="text-gray-700 hover:text-orange-500 transition-colors font-medium"
-            >
-              Ayuda
-            </a>
-            <a
-              href="#"
-              className="text-gray-700 hover:text-orange-500 transition-colors font-medium"
-            >
-              Registrar restaurante
-            </a>
+    <header className="w-full py-4 px-14">
+      <nav className="container mx-auto flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <div className="bg-white rounded-full p-2 h-16 w-16 flex items-center justify-center">
+            <span className="text-[#ff3a3a] font-bold text-xl">Miam</span>
           </div>
-
-          {isOpen && (
-            <div
-              className={`fixed top-0 right-0 h-full w-[300px] sm:w-[400px] bg-white shadow-xl z-50 transform transition-transform duration-300 ${
-                isOpen ? "translate-x-0" : "translate-x-full"
-              }`}
-            >
-              <div className="flex justify-end m-8">
-                <button
-                  onClick={() => setIsOpen(!isOpen)}
-                  className="md:hidden p-2 rounded hover:bg-gray-100 text-orange-600"
-                >
-                  ✕
-                </button>
-              </div>
-
-              <div className="flex flex-col space-y-4 mt-8 p-4">
-                <a
-                  href="#"
-                  className="text-lg font-medium text-gray-700 hover:text-orange-500 transition-colors py-2"
-                  onClick={() => setIsOpen(false)}
-                >
-                  Ayuda
-                </a>
-                <a
-                  href="#"
-                  className="text-lg font-medium text-gray-700 hover:text-orange-500 transition-colors py-2"
-                  onClick={() => setIsOpen(false)}
-                >
-                  Registrar restaurante
-                </a>
-
-                <div className=" pt-4 mt-4">2025 MIAM</div>
-              </div>
-            </div>
-          )}
-
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="md:hidden p-2 rounded hover:bg-gray-100 text-orange-600"
-          >
-            ✕
-          </button>
+          <h1 className="hidden md:flex text-white font-bold text-2xl">
+            COMÉ A TU <br/> MANERA
+          </h1>
         </div>
-      </div>
-    </nav>
+        <div className="hidden md:flex items-center gap-6">
+          <Link href="#" className="text-white hover:underline">
+            Ayuda
+          </Link>
+          <div className="h-6 border border-white/50"></div>
+          <Link href="#" className="text-white hover:underline">
+            Quiero registrar mi restaurante
+          </Link>
+        </div>
+
+        {isOpen && (
+          <div
+            className={`fixed top-0 right-0 h-full w-[300px] sm:w-[400px] bg-white shadow-xl z-50 transform transition-transform duration-300 ${
+              isOpen ? "translate-x-0" : "translate-x-full"
+            }`}
+          >
+            <div className="flex justify-end m-8">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="md:hidden p-2 rounded hover:bg-gray-100 text-orange-600"
+              >
+                ✕
+              </button>
+            </div>
+            <div className="flex flex-col space-y-4 mt-8 p-4">
+              <Link
+                href="#"
+                className="text-lg font-medium text-gray-700 hover:text-orange-500 transition-colors py-2"
+                onClick={() => setIsOpen(false)}
+              >
+                Ayuda
+              </Link>
+              <Link
+                href="#"
+                className="text-lg font-medium text-gray-700 hover:text-orange-500 transition-colors py-2"
+                onClick={() => setIsOpen(false)}
+              >
+                Quiero registrar mi restaurante
+              </Link>
+
+              <div className=" pt-4 mt-4">2025 MIAM</div>
+            </div>
+          </div>
+        )}
+
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="md:hidden hover:bg-gray-100 text-black bg-white p-2 px-4 rounded-full"
+        >
+          ✕
+        </button>
+      </nav>
+    </header>
   );
 };
 
