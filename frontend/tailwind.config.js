f614248eb3448fb08c16f040acf1716cc63faf80
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-13 15:54:20 -0300

    frontend: added Tailwind as dependency and update screens breakpoint

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
