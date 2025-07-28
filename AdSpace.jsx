3622d8ee7b2edb139ef2fb95e1fc8e9105b3aad8
gonza988 <gonzacai988@gmail.com>
2025-07-28 15:44:04 -0300

    Delete AdSpace.jsx

--- AdSpace.jsx
+++
@@ -1,7 +0,0 @@
-const AdSpace = () => (
-  <div className="mt-16 border border-dashed border-gray-600 rounded-lg p-12 text-center text-gray-400">
-    Espacio publicitario
-  </div>
-);
-
-export default AdSpace;
